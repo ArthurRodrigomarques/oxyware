983223f0f83a7869216f26529c9d9a050d98c15f
ArthurRodrigomarques <arthurrodrigo05052004@gmail.com>
2025-09-18 18:18:46 -0300

    add(sitemap)

---
+++ oxyware_frontend/next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.oxyware.com.br', 
+  generateRobotsTxt: true,              
+  sitemapSize: 5000,                     
+};
